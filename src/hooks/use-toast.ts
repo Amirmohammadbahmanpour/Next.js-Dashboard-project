f022aeafcd91cf7738ef62ca708ce808bd59b220
AmirMohammad Bahmanpour <167961093+Amirmohammadbahmanpour@users.noreply.github.com>
2026-02-03 22:14:13 +0330

    Fix ESLint error in use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -19,10 +19,10 @@ type ToasterToast = ToastProps & {
 }
 
 const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
+  ADD_TOAST: "string",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
+  REMOVE_TOAST: "string",
 } as const
 
 let count = 0
